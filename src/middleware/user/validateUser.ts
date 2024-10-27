8ac374a74914388049d28c9c68bf660ecf0ab22f
Gguife <gguife747@gmail.com>
2024-10-26 23:21:32 -0300

    refactor: Making the code adapt to middleware return request

--- src/middleware/user/validateUser.ts
+++ src/middleware/user/validateUser.ts
@@ -1,34 +1,43 @@
 import { Request, Response, NextFunction } from "express";
-import { passwordCompare, validateStrongPassword } from "../../service/user/passwordService";
+import { passwordCompare } from "../../service/user/passwordService";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const isFieldEmpty = (field: string) => !field || field.trim().length === 0;
 
-
-export const validateUserCredentialLogin = async (req: Request, res: Response, next: NextFunction) => {
+const credentialLogin = async (req: Request, res: Response, next: NextFunction) => {
   const {username, password} = req.body;
 
   if(isFieldEmpty(username) || isFieldEmpty(password)){
-    return res.status(400).json({message: 'Todos os campos são obrigatórios!'})
+    res.status(400).json({message: 'Todos os campos são obrigatórios!'})
+    return;
   }
 
   try{
     const user = await prisma.user.findUnique({ where: { username } });
-    if(!user) return res.status(404).json({error: 'Usuário não encontrado!'})
+    if(!user) {
+      res.status(404).json({error: 'Credenciais inválidas!'})
+      return;
+    }
 
     const isMatch = await passwordCompare(password, user.password);
-    if(!isMatch) return res.status(401).json({message: 'Credenciais inválidas!'})
-  
+    if(!isMatch) {
+      res.status(401).json({message: 'Credenciais inválidas!'})
+      return;
+    }
+
+    req.user = user;
+      
     next();
   }catch(error) {
-    return res.status(500).json({error: 'Error na autenticação. Tente novamente mais tarde!'})
+    console.error('Erro ao validar registro de usuário:', error);
+    res.status(500).json({error: 'Error na autenticação. Tente novamente mais tarde!'})
   }
 }
 
 
-export const validateUserCredentialRegister = async (req: Request, res: Response, next: NextFunction) => {
+const credentialRegister = async (req: Request, res: Response, next: NextFunction) => {
   const {username, password} = req.body;
   
   if (isFieldEmpty(username) || isFieldEmpty(password)) {
@@ -37,16 +46,27 @@ export const validateUserCredentialRegister = async (req: Request, res: Response
   }
 
   try {
-    const usernameVerify = await prisma.user.findUnique({ where: { username } });
+    
+    const usernameVerify = await prisma.user.findFirst({
+       where: { 
+        username: {
+          equals: username, 
+          mode: 'insensitive'
+        } 
+      } 
+    });
+
 
     if (usernameVerify) {
       res.status(400).json({ message: 'Este nome de usuário já existe!' });
       return;
     }
-    
+
     next();
   }catch(error){
-    console.error('Erro ao validar credenciais do usuário:', error);
+    console.error('Erro ao validar registro de usuário:', error);
     res.status(500).json({ error: 'Erro ao verificar nome de usuário. Tente novamente mais tarde!' });
   }
 }
+
+export default {credentialLogin, credentialRegister}
