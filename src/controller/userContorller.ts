3527ae98fc74423a1c13b7006a64bf126dfa2b4a
Gguife <gguife747@gmail.com>
2024-10-26 23:20:15 -0300

    feat: Created login and delete user controller

--- src/controller/userContorller.ts
+++ src/controller/userContorller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import { hashPassword } from "service/user/passwordService";
+import { hashPassword, validateStrongPassword } from "../service/user/passwordService";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
+
+
+const SECRET_KEY = process.env.JWT_SECRET || '';
 
 
 export const createUser = async (req: Request, res: Response) => {
   const {username, password} = req.body;
 
+
   try{
+    const validationErrosPassword = await validateStrongPassword(password)
+    if(validationErrosPassword.length > 0){
+      res.status(400).json({error: validationErrosPassword});
+      return;
+    }
 
     const hashpassword = await hashPassword(password);
 
@@ -23,5 +33,34 @@ export const createUser = async (req: Request, res: Response) => {
   }catch(error){
     console.error('Erro ao criar usuário:', error);
     res.status(500).json({ error: 'Erro ao criar usuário. Tente novamente mais tarde.' });
+  }
+}
+
+
+export const loginUser = async (req: Request, res: Response) => {
+
+  try{
+    const {id, username} = req.user!;
+
+    const token = jwt.sign({id, username}, SECRET_KEY, {expiresIn: '5h'} );
+
+    res.status(200).json({message: 'Login bem-sucedido', token: token});
+  }catch(error){
+    console.error('Erro ao realizar o login:', error);
+    res.status(500).json({ error: 'Erro ao realizar o login. Tente novamente mais tarde.' });
+  }
+}
+
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;  
+
+  try {
+    await prisma.user.delete({where: {id: parseInt(id)}});
+
+    res.status(200).json({message: 'Usuário deletado com sucesso!'})
+  }catch(error) {
+    console.error('Erro ao deletar usuário:', error);
+    res.status(500).json({ error: 'Erro ao deletar usuário. Tente novamente mais tarde.' });
   }
 }
