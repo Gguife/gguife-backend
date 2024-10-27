1ad85822f5f49d56e31311f81b2738dcd0bd5077
Gguife <gguife747@gmail.com>
2024-10-26 23:23:31 -0300

    feat: Created user routes

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from 'express';
-import { validateUserCredentialRegister } from '../middleware/user/validateUser';
-import { createUser } from 'controller/userContorller';
+import validateUser from '../middleware/user/validateUser';
+import { createUser, deleteUser, loginUser } from '../controller/userContorller';
 const router = Router();
 
 
-router.post('/register', validateUserCredentialRegister, createUser); 
+router.post('/article/user/register', validateUser.credentialRegister, createUser); 
+router.post('/article/user/login', validateUser.credentialLogin, loginUser); 
+router.post('/article/user/:id', validateUser.credentialLogin, deleteUser); 
 
 
 export default router;
