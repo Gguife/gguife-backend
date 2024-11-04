7b08a8f30f68af3dd23dea085a6fd29f76b990e8
Gguife <gguife747@gmail.com>
2024-11-04 19:26:33 -0300

    feat: Create get route for all users

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,14 @@
 import { Request, Response, Router } from 'express';
 import validateUser from '../middleware/user/validateUser';
-import { createUser, deleteUser, loginUser } from '../controller/userContorller';
+import { authToken } from '../middleware/user/authToken';
+import { createUser, deleteUser, loginUser, allUsers } from '../controller/userContorller';
 const router = Router();
 
 
 router.post('/article/user/register', validateUser.credentialRegister, createUser); 
 router.post('/article/user/login', validateUser.credentialLogin, loginUser); 
 router.post('/article/user/:id', validateUser.credentialLogin, deleteUser); 
+router.get('/article/users', authToken, allUsers); 
 
 
 export default router;
