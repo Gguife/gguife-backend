2876bc469f1946c321217cc4f78687044ac8cd58
Gguife <gguife747@gmail.com>
2024-11-15 23:50:23 -0300

    refactor: Remove unused line code

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import validateUser from '../middleware/user/validateUser';
 import { authToken } from '../middleware/user/authToken';
 import { createUser, deleteUser, loginUser, allUsers } from '../controller/userContorller';
