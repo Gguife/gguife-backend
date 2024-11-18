709fbfc35dfc5846aaffe9cce6613880c7059411
Gguife <gguife747@gmail.com>
2024-11-18 20:21:40 -0300

    feat: Add global script Multer

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,9 +1,20 @@
-import {User} from "@prisma/client"
+import {User} from "@prisma/client";
+import multer from "multer";
 
 declare global {
   namespace Express {
     interface Request {
       user?: User;
+    }
+  }
+}
+
+declare global {
+  namespace Express {
+    namespace Multer {
+      interface File {
+        location?: string;
+      }
     }
   }
 }
