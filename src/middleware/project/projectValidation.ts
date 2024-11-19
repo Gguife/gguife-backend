eb0dd79f624abeea1eee61264510e7f64fce20d6
Gguife <gguife747@gmail.com>
2024-11-18 22:47:17 -0300

    feat: Finished validation of project fields

--- src/middleware/project/projectValidation.ts
+++ src/middleware/project/projectValidation.ts
@@ -1,36 +1,32 @@
 import { Request, Response, NextFunction } from "express";
-import { PrismaClient } from "@prisma/client";
-import { validateImage } from "service/projects/imageService";
-
-const prisma = new PrismaClient();
 
 const isFieldEmpty = (field: string) => !field || field.trim().length === 0;
-const options = {
-  maxWidth: 1024,
-  maxHeight: 768,
-  maxSizeInBytes: 5 * 1024 * 1024
-}
 
-const projectValidationRegister = async (req: Request, res: Response, next: NextFunction) => {
-  const {title, content, tools, linkDeploy, linkRepository, image} = req.body;
+const projectValidate = async (req: Request, res: Response, next: NextFunction) => {
+  const {title, content, tools, linkRepository, category } = req.body;
+    
+  const requiredFields = [
+    {field: 'título', value: title},
+    {field: 'conteúdo', value: content},
+    {field: 'ferramentas', value: tools},
+    {field: 'link repository', value: linkRepository},
+    {field: 'categoria', value: category}
+  ];
 
-  if(isFieldEmpty(title) || isFieldEmpty(content) || isFieldEmpty(image) || isFieldEmpty(linkRepository)){
-    res.status(400).json({message: 'Preencha os campos obrigatórios!'})
+  for(const {field, value} of requiredFields){
+    if(isFieldEmpty(value)){
+      res.status(400).json({message: `O campo ${field} é obrigatório!`})
+      return;
+    }
+  }
+
+  if(!req.file){
+    res.status(400).json({message: 'A imagem é dobrigatória!'});
     return;
   }
 
-  try{
-
-    const isImageValid = await validateImage(image, options)
-    
-    
-
-    next();
-  }catch(error) {
-    console.error('Erro ao validar registro de projeto:', error);
-    res.status(500).json({error: 'Error na autenticação. Tente novamente mais tarde!'})
-  }
+  next();
 }
 
 
-export default projectValidationRegister;
+export default projectValidate;
