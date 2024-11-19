3427ad0e7bee3d72bf0cba025d87c8780994f914
Gguife <gguife747@gmail.com>
2024-11-18 22:49:13 -0300

    feat: Finished project routes

---
+++ src/routes/projectRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { authToken } from '../middleware/user/authToken';
+import { createProject, getProjects, getOneProject, updateProject, deleteProject } from '../controller/projectController';
+import projectValidate from '../middleware/project/projectValidation';
+import upload from '../middleware/project/multer';
+
+const router = Router();
+
+router.get('/projects', getProjects);
+router.get('/project/:id', getOneProject);
+router.post('/project', upload, projectValidate,createProject);
+router.put('/project/:id', updateProject);
+router.delete('/project/:id', deleteProject);
+
+export default router;
