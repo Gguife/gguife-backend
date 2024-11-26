90a6fd6b875e4d440caf27ec841932c7df0b1ebf
Gguife <gguife747@gmail.com>
2024-11-25 23:15:57 -0300

    refactor: Remove image dimensions code

--- src/middleware/project/multer.ts
+++ src/middleware/project/multer.ts
@@ -1,12 +1,11 @@
 import s3 from "../../config/aws";
 import multer from "multer";
 import multerS3 from "multer-s3";
-import sharp from "sharp";
 
 const storage = multerS3({
   s3,
   bucket: 'gguife-portfolio-images',
-  acl: 'public-read',
+  ACL: 'public-read',
   contentType: multerS3.AUTO_CONTENT_TYPE,
   key: (req: Express.Request, file: Express.Multer.File, cb: (error: Error | null, key?: string) => void) => {
     const fileExtension = file.originalname.split('.').pop();
@@ -26,24 +25,16 @@ const upload = multer({
     }
 
     try{
-      const buffer = (file as any).buffer;
-      const metadata = await sharp(buffer).metadata();
-
-      const maxWidth = 1024; 
-      const maxHeight = 768; 
-
-      if (metadata.width && metadata.height) {
-        if (metadata.width > maxWidth || metadata.height > maxHeight) {
-          return cb(new Error(`Dimensões máximas permitidas são ${maxWidth}x${maxHeight}.`));
-        }
+      if (!allowedTypes.includes(file.mimetype)) {
+        return cb(new Error("Tipo de arquivo inválido, apenas PNG e JPEG são permitidos."));
       }
-
       cb(null, true);
+
     }catch(e){
       cb(new Error("Falha ao processar arquivo para validação de dimensões."));
     }
   },
-}).single('file');
+}).single('imageUrl');
 
 
 export default upload;
