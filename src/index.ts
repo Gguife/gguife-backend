721bdcefbcca5133da936c84b4ac5e1c17587d4e
Gguife <gguife747@gmail.com>
2024-11-29 01:20:21 -0300

    feat: Added index project file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/userRouter";
+import projectRouter from "./routes/projectRouter";
 
-
+dotenv.config();
 const app = express();
 
-
 app.use(express.json());
+const allowedOrigins = [
+  'https://localhost:8080',
+  'http://localhost:5173'
+];
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
-app.listen(8080, () => {
-  console.log('Server running on port 8080');
+app.use(userRouter);
+app.use(projectRouter);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: '© 2024 - gguife backend online!' })
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 })
