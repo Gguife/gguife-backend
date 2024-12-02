67ae10b6ab01990e2a56fcdda7dc465a316f23dd
Gguife <gguife747@gmail.com>
2024-12-02 00:44:56 -0300

    feat: Create categories routes

--- src/routes/projectRouter.ts
+++ src/routes/projectRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authToken } from '../middleware/user/authToken';
-import { createProject, getProjects, getOneProject, updateProject, deleteProject } from '../controller/projectController';
+import { createProject, getProjects, getOneProject, updateProject, deleteProject, createCategory, getCategory } from '../controller/projectController';
 import projectValidate from '../middleware/project/projectValidation';
 import upload from '../middleware/project/multer';
 
@@ -11,5 +11,9 @@ router.get('/project/:id', getOneProject);
 router.post('/project', authToken, upload, projectValidate, createProject);
 router.put('/project/:id', authToken, projectValidate, updateProject);
 router.delete('/project/:id', authToken, deleteProject);
+
+//category
+router.post('/category', authToken, createCategory);
+router.get('/categories', getCategory);
 
 export default router;
