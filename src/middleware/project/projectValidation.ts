2028d37fb3e48a53dc0c8b4b3c60e138a2c1a684
Gguife <gguife747@gmail.com>
2024-12-01 22:19:57 -0300

    fix: Change category name in request

--- src/middleware/project/projectValidation.ts
+++ src/middleware/project/projectValidation.ts
@@ -3,14 +3,14 @@ import { Request, Response, NextFunction } from "express";
 const isFieldEmpty = (field: string) => !field || field.trim().length === 0;
 
 const projectValidate = async (req: Request, res: Response, next: NextFunction) => {
-  const {title, content, tools, linkRepository, category } = req.body;
+  const {title, content, tools, linkRepository, categories } = req.body;
     
   const requiredFields = [
     {field: 'título', value: title},
     {field: 'conteúdo', value: content},
     {field: 'ferramentas', value: tools},
     {field: 'link repository', value: linkRepository},
-    {field: 'categoria', value: category}
+    {field: 'categoria', value: categories}
   ];
 
   for(const {field, value} of requiredFields){
