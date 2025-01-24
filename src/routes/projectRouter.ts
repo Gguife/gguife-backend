a627fdc1fbfbfd56d30ba7f408262234f71ce323
Gguife <gguife747@gmail.com>
2025-01-24 00:41:31 -0300

    fix: Projects endpoints

--- src/routes/projectRouter.ts
+++ src/routes/projectRouter.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import { authToken } from '../middleware/user/authToken';
-import { createProject, getProjects, getOneProject, updateProject, deleteProject, getUserProject } from '../controller/projectController';
+import { createProject, getOneProject, updateProject, deleteProject, getProjects } from '../controller/projectController';
 import { createCategory, getCategory } from '../controller/categoryController';
 import projectValidate from '../middleware/project/projectValidation';
 import upload from '../utils/multer';
 
 const router = Router();
 
-router.get('/projects', getProjects);
 router.get('/project/:id', getOneProject);
-router.get('/projects', getUserProject);
+router.get('/projects', getProjects);
 router.post('/project', authToken, upload, projectValidate, createProject);
 router.put('/projects/:id', authToken, updateProject);
 router.delete('/projects/:id', authToken, deleteProject);
