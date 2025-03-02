2322de2a518ff4b33ee7a1d2f8acf641993c8efc
Gguife <gguife747@gmail.com>
2025-03-01 21:42:11 -0300

    test: Usecases functions finished

--- __tests__/user/usecases.test.ts
+++ __tests__/user/usecases.test.ts
@@ -1,5 +1,7 @@
 import CreateUser from '../../src/user/usecase/user.create';
 import UpdateUser from '../../src/user/usecase/user.update';
+import DeleteUser from '../../src/user/usecase/user.delete';
+import Login from '../../src/user/usecase/user.login';
 import UserRepository from '../../src/user/user.repository';
 import SendEmail from '../../src/user/usecase/email.send';
 import JWTService from '../../src/application/services/jwt.service';
@@ -18,6 +20,8 @@ describe('User requests - create, udpate, delete, login', () => {
   let sendEmail: jest.Mocked<SendEmail>;
   let createUser: CreateUser;
   let updateUser: UpdateUser;
+  let deleteUser: DeleteUser;
+  let login: Login;
   
   beforeEach(() => {
     userRepository = {
@@ -33,6 +37,8 @@ describe('User requests - create, udpate, delete, login', () => {
     sendEmail = { run: jest.fn() } as jest.Mocked<SendEmail>;
     createUser = new CreateUser(userRepository, sendEmail);
     updateUser = new UpdateUser(userRepository);
+    deleteUser = new DeleteUser(userRepository);
+    login = new Login(userRepository);
 
     (JWTService as jest.Mock).mockImplementation(() => ({
       token: jest.fn().mockReturnValue("mocked-jwt-token")
@@ -166,6 +172,79 @@ describe('User requests - create, udpate, delete, login', () => {
   })
 
 
-  //describe('Delete user',() => {})
-  //describe('login user',() => {})
+  describe('Delete user',() => {
+    const mockUser = new User(
+      1,
+      'linux',
+      'linux@gmail.com',
+      'hashedPass',
+      true,
+    );
+    
+    test('Should delete user', async () => {
+      userRepository.getById.mockResolvedValue(mockUser);
+
+      await deleteUser.run(mockUser.id);
+
+      expect(userRepository.getById).toHaveBeenCalledWith(mockUser.id);
+      expect(userRepository.delete).toHaveBeenCalledWith(mockUser.id);
+    })
+  })
+  
+  
+  describe('login user', () => {
+    const input = {
+      username: 'linux',
+      password: 'Linux123123.'
+    }
+
+    const mockUser = new User(
+      1,
+      'linux',
+      'linux@gmail.com',
+      'hashedPass',
+      true,
+    );
+    
+    //Should login and return token
+    test('Should success login and return token', async () => {
+
+      userRepository.login.mockResolvedValue(mockUser);            
+      jest.spyOn(PasswordService, 'passwordCompare').mockResolvedValue(true);
+      
+      const response = await login.run(input);
+
+      expect(userRepository.login).toHaveBeenCalledWith(input.username);
+      expect(PasswordService.passwordCompare).toHaveBeenCalledWith(input.password, mockUser.getPassword());
+      expect(mockUser.getActive()).toBe(true);
+      expect(response).toEqual({"token": "mocked-jwt-token"});
+    })
+
+    test('Should throw an error if credentials is invalid', () => {
+
+      userRepository.login.mockResolvedValue(mockUser);
+
+      jest.spyOn(PasswordService, 'passwordCompare').mockResolvedValue(false);
+
+      expect(login.run(input)).rejects.toThrow('Credentials Invalid.');
+    })
+
+    //should call getActive if user not verify and return Domain Error
+    test('Should throw an erro if user not verify', () => {
+      const mockUserInactive = new User(
+        1,
+        'linux',
+        'linux@gmail.com',
+        'hashedPass',
+        false,
+      );
+
+      userRepository.login.mockResolvedValue(mockUserInactive);
+      jest.spyOn(PasswordService, 'passwordCompare').mockResolvedValue(true);
+
+      expect(mockUserInactive.getActive()).toBe(false);
+      expect(login.run(input)).rejects.toThrow('Verify your email.');
+    })
+  })
+
 })
