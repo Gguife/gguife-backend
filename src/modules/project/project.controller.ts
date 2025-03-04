977942b623ae7c03a4ad3aa5a03ce4fc52061b1c
Gguife <gguife747@gmail.com>
2025-03-04 13:19:00 -0300

    feat: Create project create usecase and your controller

---
+++ src/modules/project/project.controller.ts
@@ -0,0 +1,37 @@
+import HttpServer from "../../infra/http/httpServe";
+import ProjectRepository from "./project.repository";
+import CreateProject from "./usecase/project.create";
+
+
+
+
+export default class ProjectController {
+
+  constructor(readonly httpServer: HttpServer, readonly projectRepository: ProjectRepository){
+  }
+
+  registerRoutes() {
+    this.projectRegister();
+  }
+
+  private projectRegister() {
+    this.httpServer.securityRoute('post', '/project/register', async (params: any, query: any, body: any, authDecoded: any) => {
+      const userId = authDecoded.id;
+
+      const input = {
+        title: body.title,
+        content: body.content,
+        tools: body.tools,
+        userId: userId,
+        categoryId: body.categoryId,
+        linkDeploy: body.linkDeploy,
+        linkRepository: body.linkRepository,
+        imageUrl: body.imageUrl
+       }
+
+       const output = await new CreateProject(this.projectRepository).run(input);
+
+       return output;
+    })
+  }
+}
---
+++ src/modules/project/project.repository.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+import Project from "./entity/Project";
+import DomainError from "application/error/DomainError";
+
+
+
+export default interface ProjectRepository {
+  create(project: Project): Promise<{id: number, title: string}>;
+}
+
+
+export class ProjectRepositoryDB implements ProjectRepository {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+
+
+  async create(project: Project): Promise<{id: number, title: string}> {
+    const projectSaved = await this.prisma.projects.create({
+      data: {
+        title: project.title,
+        content: project.content,
+        tools: project.tools,
+        linkDeploy: project.linkDeploy,
+        linkRepository: project.linkRepository,
+        imageUrl: project.imageUrl,
+        categoryId: project.categoryId,
+        userId: project.userId
+      }
+    })
+
+
+    return {
+      id: projectSaved.id,
+      title: projectSaved.title
+    }
+  }
+
+}
