7a30800a1d9f3352c00169c55cf580fd88776276
Gguife <gguife747@gmail.com>
2025-03-04 13:15:41 -0300

    feat: Add pojects register routes for endpoint of projects

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,28 @@ import dotenv from "dotenv";
 import allowedOrigin from "./application/config/cors.config";
 import { UserRepositoryDB } from "./modules/user/user.repository";
 import UserController from "./modules/user/user.controller";
+import ProjectController from "modules/project/project.controller";
+import { ProjectRepositoryDB } from "./modules/project/project.repository";
 
 dotenv.config();
 
 const httpServer = new ExpressAdapter(allowedOrigin);
 
-
+//repository - Data
 const userRepository = new UserRepositoryDB();
+const projectRepository = new ProjectRepositoryDB();
+
+//Controller - endpoints/routes
 const userController = new UserController(httpServer, userRepository);
-
-httpServer.route('get', '/', (params: any, body: any) => { return { message: "© 2025 - Gguife portfolio está Online!" } });
-
-
+const projectController = new ProjectController(httpServer, projectRepository); 
 userController.registerRoutes();
+projectController.registerRoutes();
+
+
+httpServer.route('get', '/', (params: any, body: any) => { 
+  return {
+    message: "© 2025 - Gguife portfolio está Online!"
+  } 
+});
 
 httpServer.listen(Number(process.env.PORT) || 8080);
