fc0c55ad6a6aa231efd5a7f78c2948029ee9ff49
Gguife <gguife747@gmail.com>
2025-03-04 13:18:25 -0300

    feat: Create a validateUrl function and valitaions of factory Method

--- src/modules/project/entity/Project.ts
+++ src/modules/project/entity/Project.ts
@@ -3,28 +3,68 @@ import DomainError from "../../../application/error/DomainError";
 
 const isFieldEmpty = (field: string) => !field || field.trim().length === 0;
 
+const isValidURL = (url: string) => {
+  const urlPattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+  if(!urlPattern.test(url)) return false;
+
+  try {
+    new URL(url);
+    return true;
+  }catch(err: any){
+    return false;
+  }
+}  
+
+
+
 export default class Project {
   constructor(
     readonly id: number,
     readonly title: string,
     readonly content: string,
     readonly tools: string,
-    readonly categoryId: number,
     readonly userId: number,
-    public imageUrl?: string,
-    public linkDeploy?: string,
-    public linkRepository?: string,
+    readonly categoryId: number,
+    public linkDeploy: string | null,
+    public linkRepository: string | null,
+    public imageUrl: string | null
   ){
   }
 
 
   //Factory Method for create projects
-  static create(title: string, content: string, tools: string, categoryId: number, userId: number) {
-    //Validation fields title, content, tools
-    if(isFieldEmpty(title) || isFieldEmpty(content) || isFieldEmpty(tools)) {
-      throw new DomainError('The field is empty.');
-    }
+  static create(
+    title: string,
+    content: string,
+    tools: string,
+    userId: number,
+    categoryId: number,
+    linkDeploy: string = "",
+    linkRepository: string = "",
+    imageURl: string = ""
+  ) {
 
-    return new Project(0, title, content, tools, categoryId, userId);
+    //Validation fields title, content, tools
+    if(isFieldEmpty(title)) throw new DomainError('The field title is empty.');  
+    if(isFieldEmpty(content)) throw new DomainError('The field content is empty.');  
+    if(isFieldEmpty(tools)) throw new DomainError('The field tools is empty.');  
+
+    //Validation URl for fields deploy repository and image
+    if(!isFieldEmpty(linkDeploy) && !isValidURL(linkDeploy)) throw new DomainError('The link of deploy is a invalid URL.');
+    if(!isFieldEmpty(linkRepository) && !isValidURL(linkRepository)) throw new DomainError('The repository link is a invalid URL.');
+    if(!isFieldEmpty(imageURl) && !isValidURL(imageURl)) throw new DomainError('The image URL is a invalid URL.');
+
+    return new Project(
+      0,
+      title,
+      content, 
+      tools, 
+      userId, 
+      categoryId, 
+      linkDeploy, 
+      linkRepository, 
+      imageURl
+    );
   }
 }
