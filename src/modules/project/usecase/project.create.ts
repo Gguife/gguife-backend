0ab1eb8f53e51dec0765fc24235021b611b92147
Gguife <gguife747@gmail.com>
2025-03-03 22:28:48 -0300

    feat: Create project

---
+++ src/modules/project/usecase/project.create.ts
@@ -0,0 +1,52 @@
+import Project from "../entity/Project";
+import ProjectRepository from "../project.repository";
+
+
+
+export default class CreateProject {
+
+  constructor(readonly projectRepository: ProjectRepository){
+  }
+
+  async run(input: Input): Promise<Output> {
+    // Use factory method for create project
+    const project = await Project.create(
+      input.title,
+      input.content,
+      input.tools,
+      input.userID,
+      input.categoryId,
+      input.linkDeploy,
+      input.linkRepository,
+      input.iamgeUrl
+    );
+    
+    // send response new project for projec repository create data
+    const newProject = await this.projectRepository.create(project);
+
+    // Return project ID and project title
+    return {
+      id: newProject.id,
+      title: newProject.title
+    };
+  }
+
+}
+
+
+
+type Input = {
+  title: string, 
+  content: string,
+  tools: string,
+  userID: number,
+  categoryId: number,
+  linkDeploy?: string,
+  linkRepository?: string,
+  iamgeUrl?: string
+}
+
+type Output = {
+  id: number,
+  title: string
+}
