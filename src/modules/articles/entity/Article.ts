1208d4472c19edc986a1e7ac21c518451fdbe615
Gguife <gguife747@gmail.com>
2025-03-15 01:24:18 -0300

    feat: add article entity

---
+++ src/modules/articles/entity/Article.ts
@@ -0,0 +1,60 @@
+import DomainError from "../../../application/error/DomainError";
+
+
+const isFieldEmpty = (field: string) => !field || field.trim().length === 0;
+
+const isValidURL = (url: string) => {
+  const urlPattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+  if(!urlPattern.test(url)) return false;
+
+  try {
+    new URL(url);
+    return true;
+  }catch(err: any){
+    return false;
+  }
+}  
+
+
+export default class Article {
+  constructor(
+    readonly title: string,
+    readonly introduction: string,
+    readonly content:string,
+    public imageUrl: string,
+    readonly tagId: number,
+    readonly userId: number,
+  ) {}
+
+
+  //Factory Method for create article
+  static async create(
+    title: string,
+    introduction: string,
+    content: string,
+    imageUrl: string = '',
+    tagId: number,
+    userId: number
+  ) {
+
+    //Validation fields title, introduction, content
+    if(isFieldEmpty(title)) throw new DomainError('The field title is empty.');
+    if(isFieldEmpty(introduction)) throw new DomainError('The field introduction is empty.');
+    if(isFieldEmpty(content)) throw new DomainError('The field content is empty.');
+
+    //Validation imageUrl 
+    if(!isValidURL(imageUrl)) throw new DomainError('The link of image article is a invalid URL.')
+
+    //Return article object    
+    return new Article(
+      title,
+      introduction,
+      content,
+      imageUrl,
+      tagId,
+      userId
+    )
+  }
+
+}
