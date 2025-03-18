a007f7e36b66dce7a12cdef092ceb554d8a821a0
Gguife <gguife747@gmail.com>
2025-03-18 02:27:03 -0300

    feat: add endpoint for get one article with id number

--- src/modules/articles/article.controller.ts
+++ src/modules/articles/article.controller.ts
@@ -1,6 +1,7 @@
 import HttpServer from "../../infra/http/httpServer";
 import CreateArticle from "./usecase/article.create";
 import ArticleRepository from "./article.repository";
+import GetOneArticle from "./usecase/article.getById";
 
 export default class ArticleController {
 
@@ -8,6 +9,7 @@ export default class ArticleController {
 
   registerRoutes() {
     this.articleRegister();
+    this.getArticle();
   }
 
 
@@ -31,4 +33,12 @@ export default class ArticleController {
       return output;
     })
   }
+
+  private getArticle() {
+    this.httpServer.route('get', '/article/:id', async (params: any, body: any) => {
+      const output = await new GetOneArticle(this.articleRepository).run(params.id);
+      return output; 
+    })
+  }
+  
 }
