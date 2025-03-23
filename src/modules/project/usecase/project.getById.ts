af48f713cef131ec0fa19e23bd7179b575c4ae70
Gguife <gguife747@gmail.com>
2025-03-23 04:01:28 -0300

    feat: send an array of tools instead of a single string

--- src/modules/project/usecase/project.getById.ts
+++ src/modules/project/usecase/project.getById.ts
@@ -5,9 +5,23 @@ export default class GetProject {
   constructor(readonly projectRespository: ProjectRepository){
   }
 
-  async run(id: number): Promise<Project>{
+  async run(id: number): Promise<{
+    id: number;
+    title: string;
+    introduction: string;
+    tools: string[];
+    imageUrl: string;
+    }> {
     const projectId = Number(id);
     const project = await this.projectRespository.getOne(projectId);    
-    return project;
+    const arrTools = project.tools.split(","); 
+  
+    return {
+      id: project.id,
+      title: project.title,
+      introduction: project.introduction,
+      tools: arrTools,
+      imageUrl: project.imageUrl, 
+    };
   }
 }
