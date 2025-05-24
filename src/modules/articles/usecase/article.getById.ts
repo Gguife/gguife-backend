8b4087af7d2a6a5d59accebea55ab4fcf7890f4f
Gguife <gguife747@gmail.com>
2025-05-24 12:17:12 -0300

    feat: add formatted type md for html

--- src/modules/articles/usecase/article.getById.ts
+++ src/modules/articles/usecase/article.getById.ts
@@ -1,12 +1,33 @@
 import ArticleRepository from "../article.repository";
+import {marked} from "marked";
 
 export default class GetOneArticle {
 
   constructor(readonly articleRepository: ArticleRepository){}
 
-  async run(articleId: number){
+  async run(articleId: number): Promise<Output>{
     const id = Number(articleId);
     const article = await this.articleRepository.getOne(id);
-    return article;
+    const formattedArticles = {
+      ...article,
+      content: marked.parse(article.content) as string,
+      createdAt: new Date(article.createdAt).toLocaleDateString("en-US", {
+        month: "short",
+        day: "2-digit",
+        year: "numeric"
+      })
+    };
+
+    return formattedArticles;
   }
+}
+
+type Output = {
+  id: number,
+  title: string, 
+  introduction: string,
+  content: string,
+  imageUrl: string,
+  tagId: number,
+  createdAt: string
 }
